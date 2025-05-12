40fc9696d405e17c063f1a1e65c8a8e9500047ac
juanclosada <juan_closada@soy.sena.edu.co>
2025-05-11 20:21:27 -0500

    Cambio de extension de las carpeta de HTML a PHP

--- conexion.php
+++ conexion.php
@@ -6,6 +6,7 @@ $pass = "";
 $db = "industria_2jack";
 
 $conexion = new mysqli($server, $user, $pass, $db);
+$conexion->set_charset("utf8");
 
 if($conexion->connect_errno) {
     die("conexion fallida" . $conexion->connect_errno);
